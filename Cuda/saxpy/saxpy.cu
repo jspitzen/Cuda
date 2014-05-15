57efdaa3d273c0144d48adf8d7b3ee3fafc88dd5
Jorick Spitzen <jorick1810@gmail.com>
2014-05-15 15:30:31 +0200

    Added time measurement to saxpy

--- Cuda/saxpy/saxpy.cu
+++ Cuda/saxpy/saxpy.cu
@@ -21,7 +21,10 @@ int main()
 	int size = N*sizeof(float);
 	printf("N = %d\n",N);
 
-	// Create CUDA events for 
+	// Create CUDA events for timing purposes
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
 	float *x, *y;		// Host vectors
 	float *d_x, *d_y;	// Device vectors
@@ -44,9 +47,16 @@ int main()
 	cudaMemcpy(d_y, y, size, cudaMemcpyHostToDevice);
 
 	// Perform SAXPY on 1M elements
+	cudaEventRecord(start);
 	saxpy<<<(N+255)/256, 256>>>(N, 2.0, d_x, d_y);
+	cudaEventRecord(stop);
 
 	cudaMemcpy(y, d_y, size, cudaMemcpyDeviceToHost);
+
+	cudaEventSynchronize(stop);
+
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
 
 	float maxError = 0.0f;
 	for (int i = 0; i < N; i++) 
@@ -54,4 +64,5 @@ int main()
 		maxError = max(maxError, abs(y[i]-4.0f));
 	}
 	printf("Max error: %f\n", maxError);
+	printf("Succesfully performed SAXPY on %d elements in %f milliseconds.\n", N, milliseconds);
 }
